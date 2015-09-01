352cc39ac76efe7d2cfb733f1208f5afb47699f1
Aljoscha Pörtner <aljoscha.poertner@posteo.de>
2015-09-01 17:50:07 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 chmod +w /home/hduser/.ssh
-echo -e "y\n" | ssh-keygen -t rsa -F /home/hduser/.ssh/id_rsa
+ssh-keygen -t rsa -f /home/hduser/.ssh/id_rsa
 sudo rm /home/hduser/.ssh/authorized_keys
 cat /home/hduser/.ssh/id_rsa.pub >> /home/hduser/.ssh/authorized_keys
 sudo rm -r /etc/ssh/ssh*key
