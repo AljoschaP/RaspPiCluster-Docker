91cb32154dfac363da29293ea35f887e37f77e77
Aljoscha Pörtner <aljoscha.poertner@posteo.de>
2015-09-01 17:39:19 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
-echo -e  'y\n'|sudo ssh-keygen -q -t rsa -P ""
+chmod +w /home/hduser/.ssh
+echo -e "y\n" | ssh-keygen -t rsa -F ~/.ssh/id_rsa
+sudo rm ~/.ssh/authorized_keys
 cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
 sudo rm -r /etc/ssh/ssh*key
-sudo dpkg-reconfigure openssh-server 
+sudo dpkg-reconfigure openssh-server
 sudo /etc/init.d/ssh start
