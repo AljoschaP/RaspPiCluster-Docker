3feff868058e98add1262c2a6362b71395ff4f87
Aljoscha Pörtner <aljoscha.poertner@posteo.de>
2015-09-02 10:19:27 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,3 +5,8 @@ cat /home/hduser/.ssh/id_rsa.pub >> /home/hduser/.ssh/authorized_keys
 sudo rm -r /etc/ssh/ssh*key
 sudo dpkg-reconfigure openssh-server
 sudo /etc/init.d/ssh start
+hadoop namenode -format
+/opt/hadoop/bin/start-dfs.sh
+/opt/hadoop/bin/start-mapred.sh
+alias jps='$JAVA_HOME/bin/jps'
+jps
