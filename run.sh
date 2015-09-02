b1d90eb536266bd7bb7942a45a775de25b0a9332
Aljoscha Pörtner <aljoscha.poertner@posteo.de>
2015-09-02 11:52:36 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,12 +1,28 @@
+echo "*********************************"
+echo "BEGIN SETUP HADOOP NODE"
+echo "*********************************"
 chmod +w /home/hduser/.ssh
+echo "*********************************"
+echo "GENERATING SSH KEYS"
+echo "*********************************"
 ssh-keygen -t rsa -P "" -f /home/hduser/.ssh/id_rsa
 sudo rm /home/hduser/.ssh/authorized_keys
 cat /home/hduser/.ssh/id_rsa.pub >> /home/hduser/.ssh/authorized_keys
 sudo rm -r /etc/ssh/ssh*key
+echo "*********************************"
+echo "RECONFIGURE SSH SERVER"
+echo "*********************************"
 sudo dpkg-reconfigure openssh-server
 sudo /etc/init.d/ssh start
+echo "*********************************"
+echo "SETUP AND START HADOOP"
+echo "*********************************"
 hadoop namenode -format
 /opt/hadoop/bin/start-dfs.sh
 /opt/hadoop/bin/start-mapred.sh
 alias jps='$JAVA_HOME/bin/jps'
+echo "*********************************"
+echo "END SETUP"
+echo "RESULT:"
+echo "*********************************"
 jps
